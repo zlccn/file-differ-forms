42c98ca7b48f4f706620d82ea413c02f42930761
Zhulc <7013353@qq.com>
2019-03-07 16:01:37 +0800

    简化版本优化

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ module.exports = function (opt) {
     const $appFile = _opt.appsrc.endWith('/') ? _opt.appsrc : _opt.appsrc+'/';
     const $xlsFile = _opt.xlsdir.endWith('/') ? _opt.xlsdir : _opt.xlsdir+'/';
 
+    const xlsFile = JSON.parse(fs.readFileSync($xlsFile+$mainTps).toString());
+    const appFile = JSON.parse(fs.readFileSync($appFile+$mainTps).toString());
+
+    let newFile = {};
+    for (let xls in xlsFile)
+    {
+        for (let app in appFile)
+        {
+            if(xls !== app && xlsFile[xls] === appFile[app]) newFile[app] = { value: xlsFile[xls], oldKey: xls }
+        }
+    }
+
     fs.readdir($appFile, (err, files) => {
 
         if (err) {
@@ -24,17 +36,24 @@ module.exports = function (opt) {
         {
             if(key !== $mainTps)
             {
-
                 const xlsComparisonFile = JSON.parse(fs.readFileSync($xlsFile+key).toString());
                 const appComparisonFile = JSON.parse(fs.readFileSync($appFile+key).toString());
 
                 let newComparisonFile = appComparisonFile;
-                for( let app in xlsComparisonFile)
+                for(let app in xlsComparisonFile)
                 {
                     for(let xls in appComparisonFile) {
                         if(xls === app) {
                             newComparisonFile[app] = xlsComparisonFile[app];
                         }
+                    }
+                }
+
+                for(let news in newComparisonFile)
+                {
+                    for(let old in newFile)
+                    {
+                        if(news === old) newComparisonFile[old] = xlsComparisonFile[newFile[old].oldKey]
                     }
                 }
 
